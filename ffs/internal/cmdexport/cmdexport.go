f141c6aa35d66e566ab8f21e7f25a3d11d609f7b
M. J. Fromberger <michael.j.fromberger@gmail.com>
2026-07-02 15:43:45 -0700

    ffs: consolidate export files again

--- ffs/internal/cmdexport/cmdexport.go
+++ ffs/internal/cmdexport/cmdexport.go
@@ -16,14 +16,22 @@
 package cmdexport
 
 import (
+	"archive/tar"
+	"archive/zip"
+	"bufio"
+	"errors"
+	"fmt"
+	"io"
 	"os"
 	"path/filepath"
+	"slices"
 
 	"github.com/creachadair/command"
 	"github.com/creachadair/ffs/filetree"
 	"github.com/creachadair/ffstools/ffs/config"
 	"github.com/creachadair/ffstools/lib/exportlib"
 	"github.com/creachadair/flax"
+	"github.com/klauspost/compress/zstd"
 )
 
 var exportFlags struct {
@@ -119,4 +127,110 @@ func openFlags() int {
 		return base
 	}
 	return base | os.O_EXCL
+}
+
+var zipFlags struct {
+	Root string `flag:"root,Prefix all output paths with this directory name"`
+}
+
+func runZipExport(env *command.Env, originPaths ...string) (retErr error) {
+	if exportFlags.Target == "" {
+		return env.Usagef("missing required --to path")
+	}
+	f, err := os.OpenFile(exportFlags.Target, openFlags(), 0600)
+	if err != nil {
+		return fmt.Errorf("output: %w", err)
+	}
+	defer f.Close()
+	zw := zip.NewWriter(f)
+
+	cfg := env.Config.(*config.Settings)
+	return cfg.WithStore(env.Context(), func(s filetree.Store) error {
+		ec := exportConfig(env, zipFlags.Root)
+		for _, originPath := range originPaths {
+			of, err := s.OpenPath(env.Context(), originPath)
+			if err != nil {
+				return err
+			}
+			if werr := ec.ExportToZIP(env.Context(), of, zw); werr != nil {
+				zw.Close()
+				return fmt.Errorf("copy to archive: %w", err)
+			}
+		}
+		if err := errors.Join(zw.Close(), f.Close()); err != nil {
+			return fmt.Errorf("finalize archive: %w", err)
+		}
+		return nil
+	})
+}
+
+var tarFlags struct {
+	Compress bool   `flag:"compress,Compress output with zstd"`
+	Root     string `flag:"root,Prefix all output paths with this directory name"`
+}
+
+func runTarExport(env *command.Env, originPath string, rest ...string) (retErr error) {
+	var mc mcloser
+	defer func() {
+		err := mc.Close()
+		if retErr == nil {
+			retErr = err
+		}
+	}()
+
+	// Open the output file, either stdout or the named file.
+	var w io.Writer
+	if exportFlags.Target == "" {
+		w = os.Stdout
+	} else if f, err := os.OpenFile(exportFlags.Target, openFlags(), 0700); err != nil {
+		return fmt.Errorf("output: %w", err)
+	} else {
+		mc = append(mc, f.Close)
+		w = f
+	}
+
+	// Stack an output buffer.
+	buf := bufio.NewWriter(w)
+	mc = append(mc, buf.Flush)
+	w = buf
+
+	// If enabled, stack a compressor.
+	ext := filepath.Ext(exportFlags.Target)
+	if tarFlags.Compress || ext == ".zst" || ext == ".zstd" {
+		enc, err := zstd.NewWriter(w)
+		if err != nil {
+			panic(fmt.Sprintf("zstd writer: %v", err)) // should not be possible
+		}
+		mc = append(mc, enc.Close)
+		w = enc
+	}
+	tw := tar.NewWriter(w)
+	mc = append(mc, tw.Close)
+
+	cfg := env.Config.(*config.Settings)
+	return cfg.WithStore(env.Context(), func(s filetree.Store) error {
+		ec := exportConfig(env, tarFlags.Root)
+		for _, originPath := range env.Args {
+			of, err := s.OpenPath(env.Context(), originPath)
+			if err != nil {
+				return err
+			}
+			if err := ec.ExportToTar(env.Context(), of, tw); err != nil {
+				return fmt.Errorf("export %q: %w", originPath, err)
+			}
+		}
+		return nil
+	})
+}
+
+type mcloser []func() error
+
+func (m mcloser) Close() error {
+	var errs []error
+	for _, close := range slices.Backward(m) {
+		if err := close(); err != nil {
+			errs = append(errs, err)
+		}
+	}
+	return errors.Join(errs...)
 }
